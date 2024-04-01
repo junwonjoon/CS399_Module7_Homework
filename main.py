a99504c34ccad01626c4c740e19194ca004e8f58
Wonjoon <junwonjoon41@gmail.com>
2024-03-31 21:32:13 -0700

    Rolledback requirements

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from typing import Self
 from math import sqrt
 from time import process_time
 
@@ -21,16 +22,16 @@ class Word:
         length = self.norm()
         self.vector = [x / length for x in self.vector]
 
-    def similarity(self, w) -> float:
+    def similarity(self, w: Self) -> float:
         return self * w
 
-    def __add__(self, w):
+    def __add__(self, w: Self) -> Self:
         return Word("", [x + y for x, y in zip(self.vector, w.vector)])
 
-    def __sub__(self, w):
+    def __sub__(self, w: Self) -> Self:
         return Word("", [x - y for x, y in zip(self.vector, w.vector)])
 
-    def __mul__(self, w) -> float:
+    def __mul__(self, w: Self) -> float:
         return sum([x * y for x, y in zip(self.vector, w.vector)])
 
 
